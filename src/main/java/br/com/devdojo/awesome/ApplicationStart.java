cf8328c600dfe4ade35855fe22882407c0c10f38
Jhonny <jhonnyulisilva@gmail.com>
2020-04-07 20:46:40 -0300

    Commit inicial

---
+++ src/main/java/br/com/devdojo/awesome/ApplicationStart.java
@@ -0,0 +1,14 @@
+package br.com.devdojo.awesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class ApplicationStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
+
+}
